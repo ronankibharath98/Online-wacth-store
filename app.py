42c14422e30c536c5654e0f700a4ed241f13844e
ronankibharath98 <49616979+ronankibharath98@users.noreply.github.com>
2024-02-11 23:33:00 +0000

    home page and signup page is developed

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+address = "98 lovell street, worcester, MA 01609"
 
 
 @app.route("/")
 def hello():
-  return "Hello, Folks!"
+  return render_template("home.html", contact=address)
+
+
+@app.route("/login")
+def login():
+  return render_template("login.html")
+
+
+@app.route("/signup.html")
+def signup():
+  return render_template("signup.html")
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', debug=True)
+  app.run(host="0.0.0.0", debug=True)
