37673e5df6d1ffdb6798d6c754a73456e233dd3b
ronankibharath98 <49616979+ronankibharath98@users.noreply.github.com>
2024-02-26 23:33:47 +0000

    Masking removed

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine, text
 import os
-db_connection_string = os.environ['DB_CONNECTION_STR']
+
+db_connection_string = "mysql+pymysql://mjp88b9yjlhu6akt13yp:pscale_pw_43RGrAOMlHYnONGxORJfTahDbZc8E3gP6juCf8DDt38@aws.connect.psdb.cloud/kalaonlinestore?charset=utf8mb4"
 
 engine = create_engine(db_connection_string,
                        connect_args={"ssl": {
